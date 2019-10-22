cced1aff53d65b729cd9423c34cc45798ff50afd
Martin Moravcik <martin.moravcik@decent.ch>
2019-10-22 17:47:29 +0200

    update

--- create.yaml.list.sh
+++ create.yaml.list.sh
@@ -2,17 +2,15 @@ HANDLED_FILE=$1
 REPLACE_MARK=$2
 SOURCE_VARIABLE=$(echo $@ | awk '{for (i=3; i<=NF; i++) printf $i}')
 
-# echo "Entered variable: \"${SOURCE_VARIABLE}\""
+if [ "${SOURCE_VARIABLE}" == "~" ]
+  then
+    sed -i "s/${REPLACE_MARK}/~/g" ${HANDLED_FILE}
+  else
+    FINAL_VARIABLE=$(echo ${SOURCE_VARIABLE} | sed 's/ //g' | awk -F ',' '
+    BEGIN {printf "["}
+    {for (i=1; i<NF; i++) printf "\"" $i "\", "}
+    END {printf "\"" $NF "\"]"}
+    ')
 
-FINAL_VARIABLE=$(echo ${SOURCE_VARIABLE} | sed 's/ //g' | awk -F ',' '
-BEGIN {printf "["}
-{for (i=1; i<NF; i++) printf "\"" $i "\", "}
-END {printf "\"" $NF "\"]"}
-')
-
-# echo "Final variable: ${FINAL_VARIABLE}"
-# echo "${HANDLED_FILE}"
-# echo "${REPLACE_MARK}"
-# echo "${FINAL_VARIABLE}"
-
-sed -i "s/${REPLACE_MARK}/${FINAL_VARIABLE}/g" ${HANDLED_FILE}
+    sed -i "s/${REPLACE_MARK}/${FINAL_VARIABLE}/g" ${HANDLED_FILE}
+fi
