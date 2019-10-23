15fa7e74b0af2b108b4eb508ac4764907495feea
Martin Moravcik <martin.moravcik@decent.ch>
2019-10-23 11:25:09 +0200

    update

--- create.yaml.list.sh
+++ create.yaml.list.sh
@@ -2,6 +2,8 @@ HANDLED_FILE=$1
 REPLACE_MARK=$2
 SOURCE_VARIABLE=$(echo $@ | awk '{for (i=3; i<=NF; i++) printf $i}')
 
+echo "$@" >> /tmp/bootstrap.output.txt
+
 if [ "${SOURCE_VARIABLE}" == "~" ]
   then
     sed -i "s/${REPLACE_MARK}/~/g" ${HANDLED_FILE}
